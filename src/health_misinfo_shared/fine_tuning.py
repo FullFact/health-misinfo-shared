35f74081d9950ece8396b4fd0e349e0963780132
David Corney <dcorney@gmail.com>
2024-03-14 13:11:32 +0000

    New model with controlled-vocabulary explanation field

--- src/health_misinfo_shared/fine_tuning.py
+++ src/health_misinfo_shared/fine_tuning.py
@@ -4,14 +4,16 @@
 from __future__ import annotations
 from typing import Optional
 from google.auth import default
-
-# from google.cloud import aiplatform
 import pandas as pd
 import json
 import vertexai
 from vertexai.language_models import TextGenerationModel
 from vertexai.preview.language_models import TuningEvaluationSpec
-from prompts import HEALTH_CLAIM_PROMPT, HEALTH_TRAINING_PROMPT
+from prompts import (
+    HEALTH_CLAIM_PROMPT,
+    HEALTH_TRAINING_PROMPT,
+    HEALTH_TRAINING_EXPLAIN_PROMPT,
+)
 import youtube_api
 
 credentials, _ = default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
@@ -19,6 +21,9 @@ credentials, _ = default(scopes=["https://www.googleapis.com/auth/cloud-platform
 GCP_PROJECT_ID = "exemplary-cycle-195718"
 GCP_LLM_LOCATION = "us-east4"  # NB: Gemini is not available in europe-west2 (yet?)
 GCP_TUNED_MODEL_LOCATION = "europe-west4"  # where we do fine tuning/model storage
+CHECKWORTHY_EXPLANATIONS = ["high harm", "citation", "low harm"]
+UNCHECKWORTHY_EXPLANATIONS = ["nothing to check", "hedged claim"]
+VALID_EXPLANATIONS = CHECKWORTHY_EXPLANATIONS + UNCHECKWORTHY_EXPLANATIONS
 
 
 def tuning(
@@ -59,7 +64,7 @@ def tuning(
     # Removed tensorboard - not sure how to make it work; it provides some extra visualistion during training
     # eval_spec.tensorboard = aiplatform.Tensorboard(tensorboard_name=tensorboard_instance_name)
 
-    model = TextGenerationModel.from_pretrained("text-bison@002")  # or 002?
+    model = TextGenerationModel.from_pretrained("text-bison@002")
 
     model.tune_model(
         training_data=training_data,
@@ -72,11 +77,19 @@ def tuning(
     return model
 
 
-def make_training_set() -> pd.DataFrame:
+def prepend_prompt(chunk, prompt):
+    """We need to insert the prompt as the start of each input row and
+    wrap the text in the prompt in triple-backticks."""
+    # TODO: this is now pretty trivial; not sure we need it as a function at all!
+    # as done in vertex.generate_reponse() - should probably define this just once...
+    return f"{prompt}\n```{chunk}```"
+
+
+def make_training_set_simple() -> pd.DataFrame:
     """Read a CSV file of labelled data, and create a list-of-JSON
     training set and format as a DataFrame. CSV file should have 3 columns:
     output_text (previously extracted claim), input_text (chunk of transcript), flag (True/False)
-    where True means "this should be checked" and False means "this is probably fine. 
+    where True means "this should be checked" and False means "this is probably fine.
     No header row should be given."""
     # Target data structure: list of dicts
     # [{"input_text":"prompt1","output_text":"output1"}, {"input_text"...,}, ]
@@ -91,12 +104,6 @@ def make_training_set() -> pd.DataFrame:
     training_data.columns = ["output_text", "input_text", "flag"]
     training_data["flag"] = training_data["flag"].astype("bool")
 
-    def prepend_prompt(chunk):
-        """We need to insert the prompt as the start of each input row and
-        wrap the text in the prompt in triple-backticks."""
-        # as done in vertex.generate_reponse() - should probably define this just once...
-        return f"{HEALTH_TRAINING_PROMPT}\n```{chunk}```"
-
     training_data_final = []
     grps = training_data.groupby("input_text")
     for input_text, grp in grps:
@@ -106,9 +113,55 @@ def make_training_set() -> pd.DataFrame:
         else:
             this_output = "seems legit"
         this_row = {
-            "input_text": prepend_prompt(input_text),
+            "input_text": prepend_prompt(input_text, HEALTH_TRAINING_PROMPT),
             "output_text": this_output,
         }
+        training_data_final.append(this_row)
+
+    training_data_final_df = pd.DataFrame(training_data_final)
+    return training_data_final_df
+
+
+def make_training_set_explanation() -> pd.DataFrame:
+    """(Replacing true/false flag with one of a set of explanations)
+    Read a CSV file of labelled data, and create a list-of-JSON
+    training set and format as a DataFrame. CSV file should have 3 columns:
+    output_text (previously extracted claim), input_text (chunk of transcript),
+    explanation (from fixed list of explanation)
+    where True means "this should be checked" and False means "this is probably fine.
+    No header row should be given.
+    Model will be trained to produce structured output: {"claim": <str>, "explanation: <str>}
+    See VALID_EXPLANATIONS.
+    """
+    # Target data structure: list of dicts
+    # [{"input_text":"prompt1","output_text":"output1"}, {"input_text"...,}, ]
+
+    # Load CSV with claim, source-chunk and a True/False flag
+    # We need to group all the claims (+ their flags) for each chunk.
+    # Where all the flags for a chunk are "False", that means "no need to check this", so we replace the target output with a suitable message.
+    # Where any flag is "True", that means "we need to check this" and the target output is a list of the dodgy previously-extracted claim.
+    # If some extract claims are fine but some are not, we only want the not-fine ones to be returned, so we can drop the 'false' claims entirely.
+
+    training_data = pd.read_csv("data/training_set_v2.csv")
+    training_data.columns = ["output_text", "input_text", "explanation"]
+    # training_data["flag"] = training_data["flag"].astype("bool")
+
+    training_data_final = []
+    grps = training_data.groupby("input_text")
+    for input_text, grp in grps:
+
+        this_input = prepend_prompt(input_text, HEALTH_TRAINING_EXPLAIN_PROMPT)
+        for index, row in grp.iterrows():
+            assert row["explanation"] in VALID_EXPLANATIONS
+        this_output = [
+            {"claim": row["output_text"], "explanation": row["explanation"]}
+            for index, row in grp.iterrows()
+        ]
+        this_row = {
+            "input_text": this_input,
+            "output_text": this_output,
+        }
+        # Maximum input tokens: 8192
         training_data_final.append(this_row)
 
     training_data_final_df = pd.DataFrame(training_data_final)
@@ -148,10 +201,17 @@ def get_video_responses(model, chunks: list[str]) -> None:
     all_responses = []
 
     for chunk in chunks:
-        prompt = f"{HEALTH_TRAINING_PROMPT}\n```{chunk}``` "
+        prompt = f"{HEALTH_TRAINING_EXPLAIN_PROMPT}\n```{chunk}``` "
         # To improve JSON, could append: "Sure, here is the output in JSON:\n\n{{"
-        response = model.predict(prompt)
-        # print("Candidates:\t", response.candidates, "\n")
+        # Set max_output_tokens to be higher than default to make sure the JSON response
+        # doesn't get truncated (and so become unreadable)
+        parameters = {
+            "candidate_count": 1,
+            "max_output_tokens": 2048,
+            "temperature": 0,
+            "top_p": 1,
+        }
+        response = model.predict(prompt, **parameters)
         for candidate in response.candidates:
             # candidate will be a list of 0 or more claims 'cos that's what the prompt asks for!
             try:
@@ -171,20 +231,37 @@ def get_video_responses(model, chunks: list[str]) -> None:
     return all_responses
 
 
+def pretty_format_responses(responses):
+    """Simple formatted display to console for review"""
+    for response in responses:
+        print(response["chunk"], "\n")
+        if len(response.get("claim", [])) == 0:
+            print("No claims found!")
+        else:
+            for claim in response.get("claim", []):
+                print(f">>> {claim['explanation']:20s} {claim['claim']}")
+        print("=" * 80)
+
+
 if __name__ == "__main__":
     # TODO: add simple command line options to fine-tune or load/use a model
-    
-    # Fine-tune a new model:
-    # _training_data = make_training_set()
-    # tuning("dc_tuned_3", _training_data)
-    
-    model = get_model_by_display_name("dc_tuned_3")
+    mode = "infer"
 
-    some_captions = youtube_api.load_texts("heart_disease_nat_rem")
-    
-    all_responses = []
-    for captions in some_captions[0:5]:
-        chunks = youtube_api.form_chunks(captions)
-        all_responses += get_video_responses(model, chunks)
-    print("\n\n")
-    print(all_responses)
+    if mode == "train":
+        # Fine-tune a new model:
+        # _training_data = make_training_set()
+        _training_data = make_training_set_explanation()
+        _training_data.to_json("data/train_data_v2.json", orient="records", lines=True)
+        tuning("dc_tuned_explain_0", _training_data)
+
+    if mode == "infer":
+        model = get_model_by_display_name("dc_tuned_explain_0")
+
+        some_captions = youtube_api.load_texts("heart_disease_nat_rem")
+
+        all_responses = []
+        for captions in some_captions[0:5]:
+            chunks = youtube_api.form_chunks(captions)
+            all_responses += get_video_responses(model, chunks)
+        print("\n\n")
+        pretty_format_responses(all_responses)
--- src/health_misinfo_shared/prompts.py
+++ src/health_misinfo_shared/prompts.py
@@ -49,6 +49,37 @@ HEALTH_TRAINING_PROMPT = """
             Re-write the output and return nothing except correctly formatted JSON.
             """
 
+# output will be like:
+# [{'claim': 'Saffron is as effective as Prozac in treating symptoms of depression.', 'explanation': 'high harm'},
+#  {'claim': 'another claim', 'explanation': 'nothing to check'}]
+HEALTH_TRAINING_EXPLAIN_PROMPT = """
+            Your task is to help people quickly understand a video transcript and find the most harmful claims about health being made. 
+            You should only consider claims that are on topics like health, medicine, personal health, public health, drugs, treatments or hospitals. 
+            You should estimate the potential harm of the claims.
+            
+            You will analyse the text delimited by triple backticks as follows.
+
+            Perform the following actions.
+            1 - Find up to 5 claims that are the most specific, informative and verifiable. Ignore sentences that are not directly about health. 
+            2 - Choose claims that may lead to harm to health if they are believed. This includes claims made without any scientific evidence to support them 
+            as well as claims that have a direct harm on someone's health or may harm public health.
+            3 - Explain why the claim is important using one of these labels: "high harm", "low harm", "citation", "nothing to check", "hedged claim".
+                Give the explanation "high harm" if the claim is likely to cause serious injury.
+                Give the explanation "low harm" if the claim is likely to cause minor injury.
+                Give the explanation "citation" if the claim cites scientific work in a misleading way.
+                Give the explanation "nothing to check" if the claim is true or harmless or cannit be understood.
+                Give the explanation "hedged claim" if the claim is vague or imprecise.    
+            4 - Return a list of JSON format output as follows:
+            [
+                {
+                    "claim": <claim being made in the sentence>,
+                    "explanation": <one of these labels "high harm", "low harm", "citation", "nothing to check", "hedged claim">
+                }
+            ]
+            Re-write the output and return nothing except correctly formatted JSON.
+"""
+
+
 HEALTH_CLAIM_PROMPT = """
             Your task is to process a video transcript and find any claims made about health. 
             Give the details if a health claim has been made.
