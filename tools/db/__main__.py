6b07349dd26188a41f915afb79a49a076fc87ff6
ff-dh <dakota.harris@fullfact.org>
2024-04-09 10:13:58 +0100

    just use text

--- tools/db/__main__.py
+++ tools/db/__main__.py
@@ -6,48 +6,46 @@ Table = tuple[str, dict, list[str]]
 Not overdoing this
 """
 
-def create_table(db: Connection, name: str, columns: dict, extras: list[str]) -> None:
+def create_table(db: Connection, statement: str) -> None:
     cur = db.cursor()
-
-    sql = f"CREATE TABLE {name} ("
-    sql += ", ".join(f"{column} {type}" for column, type in columns.items())
-    sql += ", ".join(["", *extras])  # head "" adds ", " if extras is not empty
-    sql += ")"
-
-    cur.execute(sql)
+    cur.execute(statement)
     db.commit()
 
+table_video_transcripts = """
+    CREATE TABLE table_video_transcripts (
+        id TEXT PRIMARY KEY,
+        url TEXT,
+        metadata TEXT,
+        transcript TEXT
+    );
+    """
 
-table_video_transcripts: Table = ("transcripts", {
-    "id": "TEXT PRIMARY KEY",
-    "url": "TEXT",
-    "metadata": "TEXT",
-    "transcript": "TEXT",
-}, [])
+table_training_claims = """
+    CREATE TABLE table_training_claims (
+        id TEXT,
+        claim TEXT, 
+        label TEXT,
+        offset_ms INTEGER,
+        FOREIGN KEY (id) REFERENCES table_video_transcripts(id)
+    );
+    """
 
-
-table_training_claims: Table = ("training_claims", {
-    "id": "TEXT",
-    "claim": "TEXT",
-    "label": "TEXT",
-    "offset_ms": "INTEGER",
-}, ["FOREIGN KEY (id) REFERENCES transcripts (id)"])
-
-
-table_inferred_claims: Table = ("inferred_claims", {
-    "id": "TEXT",
-    "claim": "TEXT",
-    "label": "TEXT",
-    "model": "TEXT",
-    "offset_ms": "INTEGER",
-}, ["FOREIGN KEY (id) REFERENCES transcripts (id)"])
-
+table_inferred_claims = """
+    CREATE TABLE table_inferred_claims (
+        id TEXT,
+        claim TEXT,
+        label TEXT, 
+        model TEXT,
+        offset_ms INTEGER,
+        FOREIGN KEY (id) REFERENCES table_video_transcripts(id)
+    );
+    """
 
 if __name__ == "__main__":
     db: Connection = sqlite3.connect("database.db")
 
-    create_table(db, *table_video_transcripts)
-    create_table(db, *table_training_claims)
-    create_table(db, *table_inferred_claims)
+    create_table(db, table_video_transcripts)
+    create_table(db, table_training_claims)
+    create_table(db, table_inferred_claims)
 
     db.close()
