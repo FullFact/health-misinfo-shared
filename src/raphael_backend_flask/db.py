a8252674385794cece609f8e518f87227bb40591
Andy Lulham <a.lulham@gmail.com>
2024-05-29 11:47:13 +0100

    refactor: label -> labels

--- src/raphael_backend_flask/db.py
+++ src/raphael_backend_flask/db.py
@@ -101,27 +101,27 @@ table_claim_extraction_runs = """
     );
     """
 
-# Note that label contains JSON data
+# Note that labels contains JSON data
 table_inferred_claims = """
     CREATE TABLE inferred_claims (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         run_id INTEGER,
         claim TEXT,
         raw_sentence_text TEXT,
-        label TEXT,
+        labels TEXT,
         offset_start_s REAL,
         offset_end_s REAL,
         FOREIGN KEY (run_id) REFERENCES claim_extraction_runs (id)
     );
     """
 
-# Note that label contains JSON data
+# Note that labels contains JSON data
 table_training_claims = """
     CREATE TABLE training_claims (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         youtube_id TEXT,
         claim TEXT,
-        label TEXT
+        labels TEXT
     );
     """
 
