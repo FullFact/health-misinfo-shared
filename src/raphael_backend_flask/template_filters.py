68d7df4935c9953bc57253bac63e080f3f759ab2
Andy Lulham <a.lulham@gmail.com>
2024-05-29 07:35:39 +0100

    Remove hours if zero

--- src/raphael_backend_flask/template_filters.py
+++ src/raphael_backend_flask/template_filters.py
@@ -2,5 +2,13 @@ import datetime
 
 
 def format_offset(seconds: float) -> str:
+    """
+    Given an offset time in seconds, returns a nicely
+    formatted string.
+    """
     seconds = int(seconds)
-    return str(datetime.timedelta(seconds=seconds))
+    formatted = str(datetime.timedelta(seconds=seconds))
+    if formatted.startswith("0:"):
+        # if hours is zero, remove it
+        formatted = formatted[2:]
+    return formatted
