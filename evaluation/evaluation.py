c91a1d59e595a03e81440db477cf61f1d1d9f2b5
Edward Dearden <ed.dearden@fullfact.org>
2024-06-07 11:54:10 +0100

    fix: fixed json parsing

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -11,6 +11,8 @@ import vertexai.preview.generative_models as generative_models
 from google.auth import default
 from vertexai.generative_models import GenerativeModel
 
+from health_misinfo_shared.data_parsing import parse_model_json_output
+
 
 credentials, _ = default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
 
@@ -96,20 +98,6 @@ class ProviderResponse:
 def load_model() -> GenerativeModel:
     vertexai.init(project=GCP_PROJECT_ID, location=GCP_LLM_LOCATION)
     return GenerativeModel(model_name=CURRENT_MODEL)
-
-
-def parse_model_json_output(model_output: str) -> list[dict[str, str]]:
-    model_output = model_output.strip()
-    if model_output.startswith("[") and model_output.endswith("]"):
-        return json.loads(model_output)
-
-    first_square_bracket_idx = model_output.find("[")
-    last_square_bracket_idx = model_output.rfind("]")
-    if first_square_bracket_idx > 0 and last_square_bracket_idx > 0:
-        return json.loads(
-            model_output[first_square_bracket_idx : last_square_bracket_idx + 1]
-        )
-    raise Exception("Could not parse the string.")
 
 
 def run_prompt(model: GenerativeModel, prompt: str) -> str:
