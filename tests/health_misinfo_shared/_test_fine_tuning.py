90e7f595282a32f83876437c32bfcee10ad21071
c-j-johnston <cameron.johnston@fullfact.org>
2024-06-20 09:58:52 +0100

    tests: update tests

--- tests/health_misinfo_shared/_test_fine_tuning.py
+++ tests/health_misinfo_shared/_test_fine_tuning.py
@@ -1,5 +1,11 @@
 from pytest import mark, param
 from health_misinfo_shared import fine_tuning
+from health_misinfo_shared.label_scoring import (
+    get_claim_summary,
+    calculate_claim_summary_score,
+    decide_claim_summary_label,
+    LABEL_SCORES,
+)
 
 
 @mark.parametrize(
@@ -26,3 +32,53 @@ def test_model_harm(chunk: str, expected_harm: str):
     ]
 
     assert model_output[0]["labels"]["harm"] == expected_harm
+
+
+@mark.parametrize(
+    "score, summary",
+    [
+        (-100, "not worth checking"),
+        (0, "not worth checking"),
+        (99, "not worth checking"),
+        (100, "not worth checking"),
+        (101, "may be worth checking"),
+        (199, "may be worth checking"),
+        (200, "may be worth checking"),
+        (201, "worth checking"),
+        (float("inf"), "worth checking"),
+    ],
+)
+def test_decide_claim_summary_label(score, summary):
+    assert decide_claim_summary_label(score) == summary
+
+
+@mark.parametrize(
+    "labels, score",
+    [
+        (
+            {
+                "understandability": "understandable",
+                "type_of_claim": "hedged claim",
+                "type_of_medical_claim": "prevention",
+                "support": "novel claim",
+                "harm": "low harm",
+            },
+            1,
+            -3,
+            5,
+            7,
+            5,
+            100,
+        )
+    ],
+)
+def test_claim_summary_score(
+    labels,
+    und_score,
+    type_claim_score,
+    med_type_score,
+    support_score,
+    harm_score,
+    total_score,
+):
+    assert calculate_claim_summary_score(labels) == total_score
