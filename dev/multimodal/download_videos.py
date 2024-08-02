621ebea677a6caa9c6dc2959621a14ac63ecc2c8
Edward Dearden <ed.dearden@fullfact.org>
2024-08-02 17:14:04 +0100

    chore: upload videos to cloud storage so they can be used by gemini

--- dev/multimodal/download_videos.py
+++ dev/multimodal/download_videos.py
@@ -1,11 +1,33 @@
 import os
 import subprocess
 
+from google.cloud import storage
+
 VIDEO_DIRECTORY = "data/videos"
 VIDEO_LIST = "data/videos/tiktoks.txt"
+GCS_BUCKET = "fullfact-nlp"
+GCS_FOLDER = "raphael/videos"
 
 
-def download_videos(video_list_path: str, video_directory_path: str):
+def upload_file_to_gcs(local_filepath: str, bucket: str, folder: str) -> None:
+    filename = local_filepath.split("/")[-1]
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket)
+    blob_location = os.path.join(folder, filename)
+    blob = bucket.blob(blob_location)
+
+    generation_match_precondition = 0
+    blob.upload_from_filename(
+        local_filepath, if_generation_match=generation_match_precondition
+    )
+
+
+def download_videos(
+    video_list_path: str,
+    video_directory_path: str,
+    gcs_bucket: str,
+    gcs_folder: str,
+):
     with open(video_list_path) as video_list_file:
         video_list = [line.strip() for line in video_list_file]
 
@@ -18,6 +40,8 @@ def download_videos(video_list_path: str, video_directory_path: str):
         print(command)
         subprocess.run(command, shell=True)
 
+        upload_file_to_gcs(out_path, gcs_bucket, gcs_folder)
+
 
 if __name__ == "__main__":
-    download_videos(VIDEO_LIST, VIDEO_DIRECTORY)
+    download_videos(VIDEO_LIST, VIDEO_DIRECTORY, GCS_BUCKET, GCS_FOLDER)
