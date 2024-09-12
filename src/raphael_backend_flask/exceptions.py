c8a86c6e112e853c4f14670a19a3475972f25c00
James McMinn <james.mcminn@fullfact.org>
2024-09-12 08:43:22 +0000

    fix: call super for flash exceptions

--- src/raphael_backend_flask/exceptions.py
+++ src/raphael_backend_flask/exceptions.py
@@ -1,4 +1,5 @@
 class FlashException(Exception):
     def __init__(self, message: str, category: str = "danger") -> None:
+        super().__init__(message)
         self.message = message
         self.category = category
